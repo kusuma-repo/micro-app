247a939047e0d8966b2a49eb29308d2e4d53d135
kusuma-repo <ifaltiga.pm@gmail.com>
2020-10-27 06:02:26 +0700

    seperate routes into router folder

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import { json } from 'body-parser';
-
+import { currentUserRouter } from './routes/Current-user';
+import { signInRouter } from './routes/Signin';
+import { signOutRouter } from './routes/Signout';
+import { signUpRouter } from './routes/Signup';
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  
-  res.send('Hi there');
-});
-
+app.use(currentUserRouter);
+app.use(signUpRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
 
 app.listen(3000, () => {
   console.log('Listening port 3000 oke');
