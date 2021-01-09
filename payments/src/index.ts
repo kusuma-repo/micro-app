534bc42bd7e0d414fecd8683786445c4c4459470
kusuma-repo <ifaltiga.pm@gmail.com>
2021-01-10 03:22:59 +0700

    remove console log

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -4,8 +4,6 @@ import { natsWrapper } from './nats.wrapper';
 import { OrderCreatedListener } from './events/listener/order.created.listener';
 import { OrderCancelledListener } from './events/listener/order.cancelled.listener';
 const start = async () => {
-  console.log('app starting up...');
-
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
